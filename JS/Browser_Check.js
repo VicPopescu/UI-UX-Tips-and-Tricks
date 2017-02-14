9e9a8b8736e9a00bc6836ac3cb80e3b7f102591a
Victor Popescu <nomevictor@gmail.com>
2017-02-14 12:49:09 +0200

    Adding description

--- JS/Browser_Check.js
+++ JS/Browser_Check.js
@@ -1,3 +1,7 @@
+/**
+ *  @description Browser check. Each variable will be initialised "true" or "false" depending on the browser found
+ *  @see bellow example
+ */
 var is_chrome = navigator.userAgent.indexOf('Chrome') > -1;
 var is_explorer = navigator.userAgent.indexOf('MSIE') > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./);
 var is_firefox = navigator.userAgent.indexOf('Firefox') > -1;
